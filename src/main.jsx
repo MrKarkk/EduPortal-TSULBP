c244386273d46706a6c5714ddd65f7ca22bf299e
MrKark <abduaziz2208@gmail.com>
2025-12-01 13:44:15 +0500

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './i18n';
 import { HashRouter } from 'react-router-dom';
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <BrowserRouter basename="/EduPortal-TSULBP">
-        <App />
-      </BrowserRouter>
+      <App />
     </HashRouter>
   </React.StrictMode>
 );
